c9defc400e865c6b7a05762b196cb4f49530892d
chsvK <chsvK21@gmail.com>
2018-07-13 09:51:48 +0530

    Working on Home-page

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="inner">
+        <nav>
+          <router-link to="/" id="logo">CHSVK</router-link>
+          <ul>
+            <li><router-link to="/home">Home</router-link></li>
+            <li><router-link to="/About">About</router-link></li>
+          </ul>
+        </nav>
+
+        <transition name="fade" mode="out-in">
+        <router-view></router-view>
+        </transition>
+      
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+<style lang="scss" src="./global.scss">
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home main-content">
+    <div class="left-content">
+      <h1>Android Development</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, odit, nulla ratione harum alias eaque nobis voluptas quasi ducimus cumque at error saepe nisi ipsum!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut quia, repudiandae laborum eum explicabo officiis.</p>
+    </div>
+    <div class="right-content img1">
+      <button><router-link to="/portfolio/one">Browse Work</router-link></button>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+
+  .img1{
+    background: url('../assets/home.jpg')
+  }
+
+</style>
